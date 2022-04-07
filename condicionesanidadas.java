c0fdf01c89b9a0d6372c1530d50ca590a1003da8
Saúliño <48542710+SaulAnto19@users.noreply.github.com>
2022-04-07 00:07:01 -0600

    add condiciones

--- condicionesanidadas.java
+++ condicionesanidadas.java
@@ -1,6 +1,24 @@
 public class condicionesanidadas {
     public static void main(String[] args) {
         int operacion = 1;
-        int num_uno
+        int num_uno = 8;
+        int num_dos = 4;
+        int resultado = 0;
+
+        if (operacion == 1) {
+            resultado = num_uno + num_dos;
+            System.out.println("El resultado de la suma es: " + resultado);
+        } else if (operacion == 2) {
+            resultado = num_uno - num_dos;
+            System.out.println("El resultado de la suma es: " + resultado);
+        } else if (operacion == 3) {
+            resultado = num_uno * num_dos;
+            System.out.println("El resultado de la suma es: " + resultado);
+        } else if (operacion == 4) {
+            resultado = num_uno / num_dos;
+            System.out.println("El resultado de la suma es: " + resultado);
+        }else{
+            System.out.println("La opcion que elegiste no existe. ");
+        }
     }
 }
