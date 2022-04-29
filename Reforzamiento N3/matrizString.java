5c12d8328e5b64e30f1297e56221f095b00009f3
Saúliño <48542710+SaulAnto19@users.noreply.github.com>
2022-04-29 00:08:53 -0600

    add matrizstring

---
+++ Reforzamiento N3/matrizString.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class matrizString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int valor=0;
+        int valorDos=0;
+
+        System.out.println("ingrese el espacio de sus filas: ");
+        valor = in.nextInt();
+        System.out.println("ingrese el espacio de sus columnas: ");
+        valorDos = in.nextInt();
+
+        String matriz[][] = new String[valor][valorDos];
+
+        System.out.println("Agregando los nombres a agregar en la matrix: ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = in.next();
+            }
+        }
+
+        System.out.println("Imprimiendo Tabla.");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println();
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+        }
+    }
+}
