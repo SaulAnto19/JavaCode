fd952046d8c5633e092ef3e1d3f7bb7af2dc9a6a
Saúliño <48542710+SaulAnto19@users.noreply.github.com>
2022-05-28 22:39:43 -0600

    add examen de ingreso de una empresa no terminado aun

---
+++ Examen/IngresosDeUnaEmpresa.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class IngresosDeUnaEmpresa {
+    public static void main(String[] args) {
+
+        Double[][] ingresos = new Double[1][12];
+
+        int opc = 0;
+
+        Scanner en = new Scanner(System.in);
+
+        do {
+            System.out.println();
+            System.out.println();
+            System.out.println("EMPRESA AMPM ");
+            System.out.println(
+                    "\n\t\t--------------------------\n"
+                            + "\t\tDIGITE LA OPCION A ESCOGER:\n\n"
+                            + "\t\t(1) registrar ingresos de los meses en el año\n"
+                            + "\t\t(2) Visualizar todos los ingresos del año\n"
+                            + "\t\t(7) salir\n"
+                            + "\t\t--------------------------");
+            System.out.print("\t\tOpcion: ");
+            opc = en.nextInt();
+            System.out.println("");
+
+            switch (opc) {
+                // registra y almacenar los datos de los ingresos de la empresa
+                case 1: {
+                    for (int i = 0; i < ingresos.length; i++) {
+                        for (int j = 0; j < ingresos[0].length; j++) {
+                            System.out.println("Ingresos del Mes N" + (j+1));
+                            ingresos[i][j] = en.nextDouble();
+                        }
+                    }
+                    break;
+                }
+                // Muestra los ingresos vendidos por mes
+                case 2: {
+                    for (int i = 0; i < ingresos.length; i++) {
+                        System.out.print("INGRESOS DEL AÑO 2019: ");
+                        for (int j = 0; j < ingresos[0].length; j++) {
+                            System.out.print("|" + ingresos[i][j] + "|");
+                        }
+                    }
+                    break;
+                }
+                default:
+                    System.out.println("");
+                    System.out.println("\t\tvuelva a elegir la opcion por favor haha");
+                    System.out.println("");
+            }
+        } while (opc != 7);
+    }
+}
