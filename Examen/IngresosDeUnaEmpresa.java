dc3679e0fae7526770e24909654c0d91ace6bfe5
Saúliño <48542710+SaulAnto19@users.noreply.github.com>
2022-05-29 16:17:24 -0600

    add avance ingresosDeUnaEmpresa

--- Examen/IngresosDeUnaEmpresa.java
+++ Examen/IngresosDeUnaEmpresa.java
@@ -7,6 +7,9 @@ public class IngresosDeUnaEmpresa {
 
         int opc = 0;
 
+        Double menor = (double) 0;
+        Double mayor = (double) 0;
+
         Scanner en = new Scanner(System.in);
 
         do {
@@ -18,6 +21,8 @@ public class IngresosDeUnaEmpresa {
                             + "\t\tDIGITE LA OPCION A ESCOGER:\n\n"
                             + "\t\t(1) registrar ingresos de los meses en el año\n"
                             + "\t\t(2) Visualizar todos los ingresos del año\n"
+                            + "\t\t(3) Determinar el mes con los mayores ingresos\n"
+                            + "\t\t(4) Determinar el mes con los menores ingresos\n"
                             + "\t\t(7) salir\n"
                             + "\t\t--------------------------");
             System.out.print("\t\tOpcion: ");
@@ -27,22 +32,60 @@ public class IngresosDeUnaEmpresa {
             switch (opc) {
                 // registra y almacenar los datos de los ingresos de la empresa
                 case 1: {
+
                     for (int i = 0; i < ingresos.length; i++) {
                         for (int j = 0; j < ingresos[0].length; j++) {
-                            System.out.println("Ingresos del Mes N" + (j+1));
+                            System.out.println("Ingresos del Mes N" + (j + 1));
                             ingresos[i][j] = en.nextDouble();
                         }
                     }
+
                     break;
                 }
                 // Muestra los ingresos vendidos por mes
                 case 2: {
+
                     for (int i = 0; i < ingresos.length; i++) {
                         System.out.print("INGRESOS DEL AÑO 2019: ");
                         for (int j = 0; j < ingresos[0].length; j++) {
                             System.out.print("|" + ingresos[i][j] + "|");
                         }
                     }
+
+                    break;
+                }
+                // Determinar el mes con los mayores ingresos
+                case 3: {
+                    mayor = ingresos[0][0];
+
+                    for (int i = 0; i < ingresos.length; i++) {
+                        for (int j = 0; j < ingresos[0].length; j++) {
+                            if (ingresos[i][j] > mayor) {
+                                mayor = ingresos[i][j];
+                            }
+                        }
+                    }
+
+                    System.out.println();
+                    System.out.println("\t\tEl ingreso mayor de de la empresa es: " + mayor);
+                    System.out.println();
+                    break;
+                }
+                // Determinar el mes con los menores ingresos
+                case 4: {
+                    menor = ingresos[0][0];
+                    for (int i = 0; i < ingresos.length; i++) {
+                        for (int j = 0; j < ingresos[0].length; j++) {
+                            if (ingresos[i][j] < menor) {
+                                menor = ingresos[i][j];
+                            }
+                        }
+                    }
+
+                    System.out.println();
+                    System.out.println("\t\tEl ingreso menor de de la empresa es: " + menor);
+                    System.out.println();
+
                     break;
                 }
                 default:
